26e24c4119fda31e4b923d61798f27fd6bce3947
StevenTsai <menghung0407@gmail.com>
2022-09-28 08:58:24 +0800

    新增 測試架構師修練之道

--- miscellaneous/app.py
+++ miscellaneous/app.py
@@ -106,22 +106,19 @@ def activity_diagram(page_number):
 
 @app.route('/test_engineer/<int:page_number>')
 def test_engineer(page_number):
-    logger.info(f'測試工程師 {page_number}')
-    previous_page = page_number - 1
-    next_page = page_number + 1
+    logger.info(f'測試架構師修練之道 {page_number}')
 
-    if previous_page < 0:
-        previous_page = 0
+    item_name = 'test_engineer'
+    limit_page = 2
 
-    if next_page > 0:
-        next_page = 0
+    template_name = get_template_name(item_name, page_number)
 
     view_model = {
-        'previous_page_route': f'/test_engineer/{previous_page}',
-        'next_page_route': f'/test_engineer/{next_page}'
+        'previous_page_route': f'/{item_name}/{get_previous_page(page_number)}',
+        'next_page_route': f'/{item_name}/{get_next_page(limit_page, page_number)}'
     }
 
-    return render_template(f'test_engineer/{page_number}.html', view_model=view_model)
+    return render_template(template_name, view_model=view_model)
 
 
 @app.route('/selenium/<int:page_number>')
@@ -188,6 +185,25 @@ def book(page_number):
     }
 
     return render_template(f'{item_name}/{page_number}.html', view_model=view_model)
+
+
+def get_template_name(item_name, page_number):
+    template_name = f'{item_name}/{page_number}.html'
+    return template_name
+
+
+def get_next_page(limit_page, page_number):
+    next_page = page_number + 1
+    if next_page > (limit_page - 1):
+        next_page = 0
+    return next_page
+
+
+def get_previous_page(page_number):
+    previous_page = page_number - 1
+    if previous_page < 0:
+        previous_page = 0
+    return previous_page
 
 
 if __name__ == '__main__':
